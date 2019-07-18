03707c0adb8c419df7277b10026ebbce36906fb9
rasoul <mahdi.alikhaani.97@gmail.com>
2019-07-18 12:18:03 +0430

    UserSelfCard 1st part

--- day4-exercise/src/Exercises/UserCard/UserSelfCard.js
+++ day4-exercise/src/Exercises/UserCard/UserSelfCard.js
@@ -1,6 +1,24 @@
-import React from 'react';
+import React from 'react'
+import styles from './userselfcard.module.css'
 
 const UserSelfCard = ({ user }) => {
-  return null;
-};
-export default UserSelfCard;
+  console.log(user)
+
+  return (
+    <div className={styles.conContainer}>
+      <div className={styles.container}>
+        <img src={user.image} alt='' />
+        <div className={styles.nameContainer}>
+          <p>{user.name}</p>
+          <p>{user.surName}</p>
+        </div>
+        <div className={styles.footer}>
+          <span>{user.gender} </span>
+          <span>*</span>
+          <span> {user.age}</span>
+        </div>
+      </div>
+    </div>
+  )
+}
+export default UserSelfCard
