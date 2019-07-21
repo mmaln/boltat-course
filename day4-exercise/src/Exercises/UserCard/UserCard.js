a8be7d35d9f3ead836f0d48224b5a684e7d88cff
rasoul <mahdi.alikhaani.97@gmail.com>
2019-07-21 22:29:00 +0430

    set user info in seperated divs

--- day4-exercise/src/Exercises/UserCard/UserCard.js
+++ day4-exercise/src/Exercises/UserCard/UserCard.js
@@ -1,9 +1,38 @@
-import React from 'react';
-import families from '../../data.json';
-import UserSelfCard from './UserSelfCard';
+import React from 'react'
+import families from '../../data.json'
+import UserSelfCard from './UserSelfCard'
+import styles from './userselfcard.module.css'
 
 const UserCard = ({ user }) => {
-  return null;
-};
+  return (
+    <div className={styles.userCardContainer}>
+      <UserSelfCard user={user} />
+      <div className={styles.marriedInfo} style={{ padding: '15px' }}>
+        <p>
+          Married To:
+          {user.marriedTo.map(el => {
+            return (
+              <div style={{ margin: '7px 7px 7px 0px' }}>
+                <a href=''>{el}</a>
+              </div>
+            )
+          })}
+        </p>
+      </div>
+      <div className={styles.childrenInfo} style={{ padding: '15px' }}>
+        <p>
+          Children:
+          {user.children.map(el => {
+            return (
+              <div style={{ margin: '7px 7px 7px 0px' }}>
+                <a href=''>{el}</a>
+              </div>
+            )
+          })}
+        </p>
+      </div>
+    </div>
+  )
+}
 
-export default UserCard;
+export default UserCard
