a23510a471225a27d5dd8d877fe41f709bbd1f8e
rasoul <mahdi.alikhaani.97@gmail.com>
2019-07-22 20:37:12 +0430

    set user card in seperated divs

--- day4-exercise/src/Exercises/UserCard/UserCard.js
+++ day4-exercise/src/Exercises/UserCard/UserCard.js
@@ -1,38 +1,30 @@
-import React from 'react'
-import families from '../../data.json'
-import UserSelfCard from './UserSelfCard'
-import styles from './userselfcard.module.css'
+import React from 'react';
+import families from '../../data.json';
+import UserSelfCard from './UserSelfCard';
+import styles from './userselfcard.module.css';
 
 const UserCard = ({ user }) => {
-  return (
-    <div className={styles.userCardContainer}>
-      <UserSelfCard user={user} />
-      <div className={styles.marriedInfo} style={{ padding: '15px' }}>
-        <p>
-          Married To:
-          {user.marriedTo.map(el => {
-            return (
-              <div style={{ margin: '7px 7px 7px 0px' }}>
-                <a href=''>{el}</a>
-              </div>
-            )
-          })}
-        </p>
-      </div>
-      <div className={styles.childrenInfo} style={{ padding: '15px' }}>
-        <p>
-          Children:
-          {user.children.map(el => {
-            return (
-              <div style={{ margin: '7px 7px 7px 0px' }}>
-                <a href=''>{el}</a>
-              </div>
-            )
-          })}
-        </p>
-      </div>
-    </div>
-  )
-}
+	return (
+		<div className={styles.userCardContainer}>
+			<UserSelfCard user={user} />
+			<div className={styles.marriedInfo} style={{ padding: '15px', margin: '20px auto' }}>
+				Married To:
+				{user.marriedTo.map(el => {
+					return <UserSelfCard user={families.find(person => person.id === el)} />;
+				})}
+			</div>
+			<div className={styles.childrenInfo} style={{}}>
+				Children:
+				{user.children.map(el => {
+					return (
+						<div style={{ border: '1px solid midnightblue', borderRadius: '15px', margin: '4px' }}>
+							<UserSelfCard user={families.find(person => person.id === el)} />
+						</div>
+					);
+				})}
+			</div>
+		</div>
+	);
+};
 
-export default UserCard
+export default UserCard;
--- day4-exercise/src/Exercises/UserCard/UserSelfCard.js
+++ day4-exercise/src/Exercises/UserCard/UserSelfCard.js
@@ -1,24 +1,24 @@
-import React from 'react'
-import styles from './userselfcard.module.css'
+import React from 'react';
+import styles from './userselfcard.module.css';
 
 const UserSelfCard = ({ user }) => {
-  console.log(user)
+	console.log(user);
+	return (
+		<div className={styles.conContainer}>
+			<div className={styles.container}>
+				<img src={user.image} alt="" />
 
-  return (
-    <div className={styles.conContainer}>
-      <div className={styles.container}>
-        <img src={user.image} alt='' />
-        <div className={styles.nameContainer}>
-          <p>{user.name}</p>
-          <p>{user.surName}</p>
-        </div>
-        <div className={styles.footer}>
-          <span>{user.gender} </span>
-          <span>*</span>
-          <span> {user.age}</span>
-        </div>
-      </div>
-    </div>
-  )
-}
-export default UserSelfCard
+				<div className={styles.nameContainer}>
+					<p>{user.name}</p>
+					<p>{user.surName}</p>
+				</div>
+				<div className={styles.footer}>
+					<span>{user.gender} </span>
+					<span>*</span>
+					<span> {user.age}</span>
+				</div>
+			</div>
+		</div>
+	);
+};
+export default UserSelfCard;
